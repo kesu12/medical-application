7e7d349354a368467209db00f7a1a44d5318e670
Alexey <drflemus@gmail.com>
2025-09-11 16:23:59 +0300

    Initial commit

---
+++ src/test/java/by/bsuir/medical_application/MedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.medical_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/by/bsuir/medicalapplication/MedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.medicalapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
